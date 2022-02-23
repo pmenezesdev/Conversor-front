c3d7f5e4d291d1ac4aa3c6e57f6bbac17a63a0f1
jan Peto <63330174+janpeto@users.noreply.github.com>
2022-02-22 23:34:42 -0300

    Anime.js
    Adição da anime.js uma engine de animação que utiliza js

--- js/calc.js
+++ js/calc.js
@@ -3,3 +3,10 @@ function converter() {
     const resultado = dol * 5.06;
     document.getElementById("BRL").value=resultado.toFixed(2);
 }
+
+anime({
+    targets: '.conv-area',
+    opacity: [0, 1],
+    duration: 1500,
+    easing: 'linear',
+});
